af8a7e0a2b661d200ec881f62f407136192cc8a9
Luis Fernando Prudencio Cruz <iscluis@hotmail.com>
2021-05-18 13:44:45 -0500

    feat: Commit inicial, Se agregan componentes y se configur ruteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'employees',
+    loadChildren: () =>
+      import('./modules/employees/employees.module').then(
+        ({ EmployeesModule }) => EmployeesModule
+      ),
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'employees',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/modules/employees/screens/list-employees/list-employees.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-employees',
+  templateUrl: './list-employees.component.html',
+  styleUrls: ['./list-employees.component.scss']
+})
+export class ListEmployeesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
