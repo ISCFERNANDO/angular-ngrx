0bac56d6196578b71150d0ff05ba27eeaba873bd
Luis Fernando Prudencio Cruz <iscluis@hotmail.com>
2021-05-19 07:59:30 -0500

    feat: Se agregan filtros en tablas, avance de detalle de empleado

--- src/app/modules/employees/screens/list-employees/list-employees.component.ts
+++ src/app/modules/employees/screens/list-employees/list-employees.component.ts
@@ -3,6 +3,24 @@ import { Employee } from '../../models/employee.model';
 import { ToastrService } from 'ngx-toastr';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/redux/state';
+import { MatDialog } from '@angular/material/dialog';
+import { DetailsEmployeeComponent } from '../details-employee/details-employee.component';
+
+import { ClrDatagridFilterInterface } from '@clr/angular';
+import { Subject } from 'rxjs';
+
+export class GridFilters implements ClrDatagridFilterInterface<Employee> {
+  changes = new Subject<any>();
+  isActive(): boolean {
+    console.log('is active');
+    return true;
+  }
+
+  accepts(employee: Employee) {
+    console.log(employee);
+    return true;
+  }
+}
 
 @Component({
   selector: 'app-list-employees',
@@ -13,10 +31,12 @@ export class ListEmployeesComponent implements OnInit {
   employees: Employee[] = [];
   totalItems: number = 0;
   loading: boolean = false;
+  filter: GridFilters = new GridFilters();
 
   constructor(
     private toastService: ToastrService,
-    private store: Store<AppState>
+    private store: Store<AppState>,
+    private dialog: MatDialog
   ) {}
 
   ngOnInit(): void {
@@ -33,6 +53,15 @@ export class ListEmployeesComponent implements OnInit {
       this.employees = data.employees;
       this.totalItems = this.employees.length;
       this.loading = false;
+    });
+  }
+
+  openDetail(employee: Employee): void {
+    this.dialog.open(DetailsEmployeeComponent, {
+      width: '650px',
+      data: employee,
+      panelClass: 'custom-dialog',
+      disableClose: true,
     });
   }
 }
--- src/app/shared/components/avatar-upload/avatar-upload.component.ts
+++ src/app/shared/components/avatar-upload/avatar-upload.component.ts
@@ -1,47 +1,52 @@
 import {
-    Component,
-    EventEmitter,
-    Input,
-    OnChanges,
-    OnInit,
-    Output
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
 } from '@angular/core';
 
 @Component({
-    selector: 'app-avatar-upload',
-    templateUrl: './avatar-upload.component.html',
-    styleUrls: ['./avatar-upload.component.scss']
+  selector: 'app-avatar-upload',
+  templateUrl: './avatar-upload.component.html',
+  styleUrls: ['./avatar-upload.component.scss'],
 })
 export class AvatarUploadComponent implements OnInit, OnChanges {
-    @Input() imageUrl?: string;
-    @Output() fileEvent: EventEmitter<File> = new EventEmitter();
-    file: File | any;
-    imgURL: any;
-    constructor() {}
+  @Input() imageUrl?: string;
+  @Input() isEditable: boolean = true;
+  @Output() fileEvent: EventEmitter<File> = new EventEmitter();
+  @Input() file: File | any;
+  imgURL: any;
+  constructor() {}
 
-    ngOnInit(): void {}
-
-    ngOnChanges(): void {
-        if (this.imageUrl) {
-            this.file = null;
-            this.imgURL = '';
-        }
+  ngOnInit(): void {
+    if (this.file) {
+      this.preview();
     }
+  }
 
-    onSelect(event: any) {
-        this.file = event.addedFiles[0];
-        this.fileEvent.emit(this.file);
-        this.preview();
+  ngOnChanges(): void {
+    if (this.imageUrl) {
+      this.file = null;
+      this.imgURL = '';
     }
+  }
 
-    preview() {
-        if (!this.file) return;
-        var mimeType = this.file.type;
-        if (mimeType.match(/image\/*/) == null) return;
+  onSelect(event: any) {
+    this.file = event.addedFiles[0];
+    this.fileEvent.emit(this.file);
+    this.preview();
+  }
 
-        var reader = new FileReader();
-        reader.readAsDataURL(this.file);
-        reader.onload = (_event) => (this.imgURL = reader.result);
-        this.file = null;
-    }
+  preview() {
+    if (!this.file) return;
+    var mimeType = this.file.type;
+    if (mimeType.match(/image\/*/) == null) return;
+
+    var reader = new FileReader();
+    reader.readAsDataURL(this.file);
+    reader.onload = (_event) => (this.imgURL = reader.result);
+    this.file = null;
+  }
 }
